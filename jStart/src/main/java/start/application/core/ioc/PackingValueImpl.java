36c2d477058d60bd596a14f776a6a6476328dc2b
Start <4544242@qq.com>
2017-03-12 14:20:27 +0800

    创建自定义对象

--- jStart/src/main/java/start/application/core/ioc/PackingValueImpl.java
+++ jStart/src/main/java/start/application/core/ioc/PackingValueImpl.java
@@ -4,8 +4,17 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import start.application.core.ApplicationIO;
+import start.application.core.exceptions.ApplicationException;
 
 public class PackingValueImpl {
+	
+	public Object newInstance(Class<?> type,String name){
+		try {
+			return type.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new ApplicationException(e);
+		}
+	}
 
 	public Object getValue(Field field,Method method,Class<?> type,String key){
 		return ApplicationIO.read(null, method, type, key);
